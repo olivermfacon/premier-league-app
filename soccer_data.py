ba6fb310b8601de8c6b216818afa6dc3789f3711
Oliver Facon <omf11@georgetown.edu>
2020-05-03 19:30:37 +0100

    Basic odds setup

--- soccer_data.py
+++ soccer_data.py
@@ -11,7 +11,6 @@ def club_colors(selected_team_id):
     connection.request('GET', f'/v2/teams/{selected_team_id}', None, headers )
     response = json.loads(connection.getresponse().read().decode())
     color = response["clubColors"]
-    print(color)
     y=0
     for char in color:
         if char == '/':
@@ -25,11 +24,13 @@ def club_colors(selected_team_id):
             colors[1] = color
     if response["name"] == "Manchester City FC":
         colors[0] = "#1CC6E8"
-    print(response["name"])
+    elif response["name"] == "Wolverhampton Wanderers FC":
+        colors[1] = "#FDB913"
+    elif response["name"] == "West Ham United FC":
+        colors[1] = "#7A263A"
+        colors[0] = "#1BB1E7"
     return colors
     
-    
-
 def format_date(match_date):
     return match_date[0:10]
 
@@ -42,6 +43,7 @@ def get_menu_option():
     print("6. View season statistics...")
     print("7. View team information...")
     print("8. Sign up to your club's weekly newsletter...")
+    print("9. Calculate odds on next game...")
     print()
     return input("CHOOSE AN OPTION BELOW BY ENTERING THE MENU NUMBER: ")
 
@@ -221,26 +223,33 @@ def season_statistics(team_stats):
     print("\tGoals Conceded: " + str(team_stats["goalsAgainst"]))
     print("\tGoal Difference: " + str(team_stats["goalDifference"]) + "\n")
 
-def team_info(team):
-    print("\n" + team["name"].upper() + "\n")
-    print("\t" + "FOUNDED: " + str(team["founded"]))
-    print("\t" + "VENUE: " + team["venue"])
-    print("\t" + "CLUB COLORS: " + team["clubColors"] + "\n")
-    print("\t" + "ACTIVE COMPETITIONS:")
-    for competition in team["activeCompetitions"]:
-        print("\t   " + competition["name"])
-    print("\n\t" + "ADDRESS: " + team["address"])
-    print("\t" + "PHONE: " + team["phone"])
-    print("\t" + "WEBSITE: " + team["website"])
-    print("\t" + "EMAIL: " + team["email"] + "\n")
+def team_info(team, purpose):
+    if purpose == "console":
+        print("\n" + team["name"].upper() + "\n")
+        print("\t" + "FOUNDED: " + str(team["founded"]))
+        print("\t" + "VENUE: " + team["venue"])
+        print("\t" + "CLUB COLORS: " + team["clubColors"] + "\n")
+        print("\t" + "ACTIVE COMPETITIONS:")
+        for competition in team["activeCompetitions"]:
+            print("\t   " + competition["name"])
+        print("\n\t" + "ADDRESS: " + team["address"])
+        if team["phone"] != None:
+            print("\t" + "PHONE: " + team["phone"])
+        print("\t" + "WEBSITE: " + team["website"])
+        if team["email"] != None:
+            print("\t" + "EMAIL: " + team["email"])
+        print()
+    elif purpose == "email":
+        team_contacts = [team["address"], team["phone"], team["website"]]
+        if team["email"] != None:
+            team_contacts.append(team["email"])
+        return team_contacts
 
-def newsletter(next_content, last_content, requested_team, status, selected_team_id):
+def newsletter(next_content, last_content, requested_team, selected_team_id, team_contact):
     color_theme = club_colors(selected_team_id)
-    print(color_theme[0])
-    print(color_theme[1])
     x = 0
-    last_body = "<b>THE LAST FIVE GAMES</b><br/><br/>"
-    next_body = "<b>THE NEXT FIVE GAMES</b><br/><br/>"
+    last_body = "<b><u>THE LAST FIVE GAMES</u></b><br/><br/>"
+    next_body = "<b><u>THE NEXT FIVE GAMES</u></b><br/><br/>"
     while(x<10):
         last_body += f"{last_content[x]}<br/>{last_content[x+1]}<br/><br/>"
         next_body += f"{next_content[x]}<br/>{next_content[x+1]}<br/><br/>"
@@ -249,12 +258,20 @@ def newsletter(next_content, last_content, requested_team, status, selected_team
         x = color_theme[0]
         color_theme[0] = color_theme[1]
         color_theme[1] = x
+    footer_para = f"<div>Phone: {team_contact[1]}</div>"
+    if team_contact[3] != None:
+        footer_para += f"<div>Email: {team_contact[3]}</div>"
     message = f"""<html>
                 <body style="font-family:'Verdana';text-align:center;color:{color_theme[0]};background-color:{color_theme[1]};">
-                    <h1 style="color:{color_theme[1]};background-color:{color_theme[0]};font-size:30px;">{requested_team} NEWSLETTER</h1>
+                    <h1 style="color:{color_theme[1]};background-color:{color_theme[0]};font-size:30px;border:3px;border-style:solid;border-color:{color_theme[1]};">{requested_team} NEWSLETTER</h1>
                     <p style="font-size:13px;">{last_body}</p>
                     <p style="font-size:13px;">{next_body}</p>
                 </body>
+                <footer style="font-family:'Verdana';text-align:center;color:{color_theme[0]};background-color:{color_theme[1]};">
+                    <p><u>Contact your Team</u></p>
+                    <div>Address: {team_contact[0]}</div><div>Website: {team_contact[2]}</div>
+                    {footer_para}
+                </footer>
             </html>"""
     email_api_key = os.environ.get("API_KEY")
     api_secret = os.environ.get("API_SECRET")
@@ -282,6 +299,27 @@ def newsletter(next_content, last_content, requested_team, status, selected_team
     result = mailjet.send.create(data=data)
     print(result.status_code)
     print(result.json())
+
+def form(selected_id):
+    connection.request('GET', f'/v2/teams/{selected_team_id}/matches?status=FINISHED', None, headers )
+    response = json.loads(connection.getresponse().read().decode())
+
+    for match in response["matches"]:
+        if match["competition"]["name"] == "Premier League":
+            finished_matches.append(match)
+
+    num_games = len(finished_matches)
+    x=1
+    form = 0
+    while(x<6):
+        if finished_matches[num_games-x]["score"]["winner"] == "HOME_TEAM" and finished_matches[num_games-x]["homeTeam"]["id"] == selected_id:
+            form += 9
+        elif finished_matches[num_games-x]["score"]["winner"] == "AWAY_TEAM" and finished_matches[num_games-x]["awayTeam"]["id"] == selected_id:
+            form += 9
+        elif finished_matches[num_games-x]["score"]["winner"] == "None":
+            form += 3
+        x+=1
+    return form
 
 team_names = []
 short_names = []
@@ -394,12 +432,14 @@ while menu_selection!="done":
             x += 1
         
     elif menu_selection == "7":
+        purpose = "console"
         connection.request('GET', f'/v2/teams/{selected_team_id}', None, headers )
         response = json.loads(connection.getresponse().read().decode())
         team = response
-        team_info(team)
+        team_info(team, purpose)
     
     elif menu_selection == "8":
+        matches = []
         purpose = "email"
         connection.request('GET', f'/v2/teams/{selected_team_id}/matches?status=FINISHED', None, headers )
         response = json.loads(connection.getresponse().read().decode())
@@ -429,8 +469,83 @@ while menu_selection!="done":
                 if match["competition"]["name"] == "Premier League":
                     matches.append(match)     
         next_content = next_five(matches, status, purpose)
+
+        connection.request('GET', f'/v2/teams/{selected_team_id}', None, headers )
+        response = json.loads(connection.getresponse().read().decode())
+        team = response
+        team_contact = team_info(team, purpose)
         
-        newsletter(next_content, last_content, requested_team, status, selected_team_id)
+        newsletter(next_content, last_content, requested_team, selected_team_id, team_contact)
+
+    elif menu_selection == "9":
+        finished_matches = []
+
+        connection.request('GET', f'/v2/teams/{selected_team_id}/matches?status=POSTPONED', None, headers )
+        response = json.loads(connection.getresponse().read().decode())
+
+        for match in response["matches"]:
+            if match["competition"]["name"] == "Premier League":
+                matches.append(match)
+        next_game = matches[0]
+        ids_names = [str(next_game["homeTeam"]["id"]), next_game["homeTeam"]["name"], str(next_game["awayTeam"]["id"]), next_game["awayTeam"]["name"]]
+        if selected_team_id == ids_names[0]:
+            home_bonus = 5
+        else:
+            home_bonus = -5
+        
+        y = 0 
+        z = 0
+
+        selected_team_id = str(selected_team_id)
+
+        for x in ids_names:
+            if x == selected_team_id:
+                team_id = selected_team_id
+                z = y
+            y+=1
+
+        if z == 0:
+            opp_id = ids_names[2]
+        else:
+            opp_id = ids_names[0]
+        
+        team_form = form(team_id)
+        opp_form = form(opp_id)
+        
+        connection.request('GET', '/v2/competitions/PL/standings', None, headers )
+        response = json.loads(connection.getresponse().read().decode())
+        standings = response["standings"][0]["table"]
+
+        for team in standings:
+            print(team_id)
+            print(team["team"]["id"])
+            if str(team["team"]["id"]) == team_id:
+                print("hi")
+                team_points = team["points"]
+            elif str(team["team"]["id"]) == opp_id:
+                opp_points = team["points"]
+
+        team_odds_tally = home_bonus + team_form + team_points
+        opp_odds_tally = opp_form + opp_points
+
+        win_prob = team_odds_tally / (opp_odds_tally + team_odds_tally)
+        
+        if odds < 0.5:
+            american_odds = (100/win_prob) - 100
+            american_odds = round(american_odds, 0)
+            american_odds = "+" + str(american_odds)
+        else:
+            american_odds = (win_prob*100 / (1 - win_prob))*-1
+            american_odds = round(american_odds, 0)
+            american_odds = str(american_odds)
+        
+        american_odds = american_odds[:4]
+        
+        print(american_odds)
+        
+        
+        
+
 
     menu_selection = get_menu_option()
 
